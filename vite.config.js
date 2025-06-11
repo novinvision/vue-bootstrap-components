066939386269697680515185807bb33897a0686d
Mehran Ranji <mehran.ranji@gmail.com>
2025-06-11 14:44:01 +0330

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.js'),
+            name: 'Vue3BootstrapComponents',
+            fileName: () => 'vue3-bootstrap-components.mjs',
+            formats: ['es']
+        },
+        rollupOptions: {
+            external: ['vue', 'bootstrap'], // اجزای خارجی
+        }
+    }
+});

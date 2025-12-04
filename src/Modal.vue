151833283bdce932e18a1c684c25ef1d295fe11f
Mehran Ranji <mehran.ranji@gmail.com>
2025-12-04 13:05:44 +0330

    modal emits

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,10 +1,11 @@
 <template>
   <div
       :id="name"
+      ref="modal"
       class="modal">
     <div class="modal-dialog modal-dialog-centered" :class="{'modal-fullscreen' : fullscreen, [dialogClass]: true}">
       <div class="modal-content">
-        <slot/>
+        <slot :modal="modal"/>
       </div>
     </div>
   </div>
@@ -36,16 +37,26 @@ export default {
   },
   expose: ['close'],
   mounted() {
+    let _this = this;
     this.modal = new Modal(document.getElementById(this.name),{
       backdrop: true,
     })
+
+    this.$refs.modal.addEventListener('hidden.bs.modal', event => {
+      _this.$emit('closed', event);
+    })
+
+    this.$refs.modal.addEventListener('shown.bs.modal', event => {
+      _this.$emit('opened', event);
+    })
+
   },
   beforeUnmount() {
-    this.modal.hide();
+    this.modal?.hide();
   },
   methods: {
     close() {
-      this.modal.hide();
+      this.modal?.hide();
     }
   },
   data(){

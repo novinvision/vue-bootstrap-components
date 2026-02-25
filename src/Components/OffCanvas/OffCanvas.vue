8f2a9b8ca770d259d301ff4d7618dd51ea2018ea
Mehran Ranji <mehran.ranji@gmail.com>
2026-02-25 18:59:41 +0330

    inertia router event

--- src/Components/OffCanvas/OffCanvas.vue
+++ src/Components/OffCanvas/OffCanvas.vue
@@ -1,7 +1,15 @@
 <script setup>
-import { ref, defineExpose } from 'vue'
+import {ref, defineExpose, defineEmits, onMounted, onBeforeUnmount} from 'vue'
 import Offcanvas from 'bootstrap/js/dist/offcanvas'
 import { useBootstrapInstance } from '../../Composables/useBootstrapInstance'
+
+const emit = defineEmits([
+  'hide',
+  'hidden',
+  'hidePrevented',
+  'show',
+  'shown',
+])
 
 const props = defineProps({
   name: {
@@ -18,6 +26,29 @@ const show = () => getInstance()?.show()
 const hide = () => getInstance()?.hide()
 
 defineExpose({ show, hide })
+
+const emitHide = (event) => emit('hide', event)
+const emitHidden = (event) => emit('hidden', event)
+const emitHidePrevented = (event) => emit('hidePrevented', event)
+const emitShow = (event) => emit('show', event)
+const emitShown = (event) => emit('shown', event)
+
+onMounted(() => {
+  el.value.addEventListener('hide.bs.offcanvas', emitHide)
+  el.value.addEventListener('hidden.bs.offcanvas', emitHidden)
+  el.value.addEventListener('hidePrevented.bs.offcanvas', emitHidePrevented)
+  el.value.addEventListener('show.bs.offcanvas', emitShow)
+  el.value.addEventListener('shown.bs.offcanvas', emitShown)
+})
+
+onBeforeUnmount(() => {
+  el.value.removeEventListener('hide.bs.offcanvas', emitHide)
+  el.value.removeEventListener('hidden.bs.offcanvas', emitHidden)
+  el.value.removeEventListener('hidePrevented.bs.offcanvas', emitHidePrevented)
+  el.value.removeEventListener('show.bs.offcanvas', emitShow)
+  el.value.removeEventListener('shown.bs.offcanvas', emitShown)
+})
+
 </script>
 <template>
   <div class="offcanvas" tabindex="-1" :id="props.name" :aria-labelledby="props.name + 'Label'" ref="el">
--- src/Composables/useBootstrapInstance.js
+++ src/Composables/useBootstrapInstance.js
@@ -1,18 +1,36 @@
-import { onMounted, onBeforeUnmount } from 'vue'
+import {onMounted, onBeforeUnmount} from 'vue'
 
 export function useBootstrapInstance(refEl, BootstrapClass, options = {}) {
     let instance = null
 
     onMounted(() => {
         instance = BootstrapClass.getOrCreateInstance(refEl.value, options)
+
+        if(window){
+            document.addEventListener('inertia:start', closeEl)
+        }
     })
 
     onBeforeUnmount(() => {
+        if (window) {
+            document.removeEventListener('inertia:start', closeEl)
+        }
+
+        disposeEl()
+    })
+
+    const disposeEl = function () {
         if (instance) {
             instance.dispose()
             instance = null
         }
-    })
+    }
+
+    const closeEl = function () {
+        if (instance && typeof instance.hide === "function") {
+            instance.hide()
+        }
+    }
 
     return {
         getInstance: () => instance
